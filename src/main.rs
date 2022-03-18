ef1d888515577ba24c4ada902040d55035653384
Forrest Jacobs <forrestjacobs@gmail.com>
2022-03-18 00:04:25 -0400

    Set intents

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod config;
 mod handler;
 
+use serenity::client::bridge::gateway::GatewayIntents;
 use serenity::client::Client;
 use serenity::model::id::GuildId;
 
@@ -11,6 +12,7 @@ async fn main() {
     let config = get_config();
 
     let mut client = Client::builder(config.discord_token)
+        .intents(GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES)
         .event_handler(handler::Handler {
             guild_id: GuildId(config.guild_id),
             services: config.services,
