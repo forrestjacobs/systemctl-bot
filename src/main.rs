efa5d6c2c88db87bb767a05038ce3fdca0a80ea3
Forrest Jacobs <forrestjacobs@gmail.com>
2022-04-18 16:46:11 -0400

    Update serenity and add serenity-cache

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod systemctl;
 
 use crate::config::get_config;
 use clap::Parser;
-use serenity::client::bridge::gateway::GatewayIntents;
 use serenity::client::Client;
+use serenity::model::gateway::GatewayIntents;
 use serenity::model::id::GuildId;
 
 #[derive(Parser, Debug)]
@@ -22,15 +22,17 @@ async fn main() {
 
     let config = get_config(args.config).await.unwrap();
 
-    let mut client = Client::builder(config.discord_token)
-        .intents(GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES)
-        .event_handler(handler::Handler {
-            guild_id: GuildId(config.guild_id),
-            units: config.units,
-        })
-        .application_id(config.application_id)
-        .await
-        .unwrap();
+    let mut client = Client::builder(
+        config.discord_token,
+        GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES,
+    )
+    .event_handler(handler::Handler {
+        guild_id: GuildId(config.guild_id),
+        units: config.units,
+    })
+    .application_id(config.application_id)
+    .await
+    .unwrap();
 
     client.start().await.unwrap();
 }
