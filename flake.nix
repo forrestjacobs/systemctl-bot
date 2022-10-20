c2af62b581fa3e65b98b2ece1fcca16cf1007d0f
Forrest Jacobs <forrestjacobs@gmail.com>
2022-10-20 13:42:59 -0400

    Add nix flake for package

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Control your Linux server from Discord";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      package = pkgs.rustPlatform.buildRustPackage {
+        pname = "systemctl-bot";
+        version = "0.4.1";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+    in {
+      packages.systemctl-bot = package;
+      packages.default = package;
+    });
+
+}
