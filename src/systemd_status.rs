a0dd5b8e72c132c2c03f8e58c8cb6cc1daa24ec5
Forrest Jacobs <forrestjacobs@gmail.com>
2023-10-17 19:51:01 -0400

    Clean up systemd_status

--- src/systemd_status.rs
+++ src/systemd_status.rs
@@ -37,23 +37,17 @@ impl SystemdStatusManager {
     }
 
     pub async fn status_stream(&self, unit: &str) -> Result<PropertyStream<'_, String>, Error> {
-        Ok(self
-            .client
-            .load_unit(unit)
-            .await?
-            .receive_active_state_changed()
-            .await)
-    }
-
-    async fn status_with_name<'a>(&self, unit: &'a str) -> (&'a str, Result<String, Error>) {
-        (unit, self.status(unit).await)
+        let unit = self.client.load_unit(unit).await?;
+        Ok(unit.receive_active_state_changed().await)
     }
 
     pub async fn statuses<'a, I: Iterator<Item = &'a str>>(
         &self,
         units: I,
     ) -> Vec<(&'a str, Result<String, Error>)> {
-        let statuses = units.map(|unit| self.status_with_name(unit));
-        join_all(statuses).await
+        let units: Vec<&str> = units.collect();
+        let statuses = units.iter().map(|unit| self.status(unit));
+        let statuses = join_all(statuses).await;
+        units.into_iter().zip(statuses).collect()
     }
 }
